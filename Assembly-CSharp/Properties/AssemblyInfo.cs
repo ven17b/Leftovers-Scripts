c23d7811a4f60ac54ba5dc2427532771c9fcf328
SpanishFreddy <121837347+fr3dsp@users.noreply.github.com>
2025-12-31 12:56:02 +0100

    Some stuff out.

--- Assembly-CSharp/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Configuration.Assemblies;
-using System.Reflection;
-
-[assembly: AssemblyAlgorithmId(AssemblyHashAlgorithm.None)]
-[assembly: AssemblyVersion("0.0.0.0")]
